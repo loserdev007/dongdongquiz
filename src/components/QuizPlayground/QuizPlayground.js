bd38e4a9528e22343980e13d1c89362ca7590864
loserdev007 <62440723+loserdev007@users.noreply.github.com>
2022-10-14 14:22:11 +0900

    Question screen UI design change

--- src/components/QuizPlayground/QuizPlayground.js
+++ src/components/QuizPlayground/QuizPlayground.js
@@ -1,73 +1,83 @@
 import "./QuizPlayground.css";
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import SingleQuestion from "./SingleQuestion/SingleQuestion";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/esm/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+let currQ = 0;
+let ras = 0;
 
 const QuizPlayground = () => {
    const data = useLoaderData().data;
    const questions = data.questions;
    // console.log(questions[0].options.length);
-   console.log("aaa", questions);
-   let leftgone = 0;
-   let currQ = 1;
+   // let leftgone = 0;
+
+   const [move, setMove] = useState(0);
 
    const nextBtnClickHandler = (e) => {
-      if (leftgone + 100 / questions.length < 100) {
-         e.target
-            .closest(".quiz-main")
-            .querySelector(
-               "#questions-wrapper"
-            ).style.transform = `translateX(-${
-            leftgone + 100 / questions.length
-         }%)`;
-         leftgone = leftgone + 100 / questions.length;
+      // if (leftgone + 100 / questions.length < 100) {
+      //    e.target
+      //       .closest(".quiz-main")
+      //       .querySelector(
+      //          "#questions-wrapper"
+      //       ).style.transform = `translateX(-${
+      //       leftgone + 100 / questions.length
+      //    }%)`;
+      //    leftgone = leftgone + 100 / questions.length;
+      //    currQ++;
+      //    e.target
+      //       .closest(".quiz-main")
+      //       .querySelector(".extras #current-question").textContent = currQ;
+      //    e.target.closest(".quiz-main").style.backgroundColor = "#292a2e";
+      //    console.log(leftgone);
+      // }
+      if (currQ + 1 < questions.length) {
          currQ++;
-         e.target
-            .closest(".quiz-main")
-            .querySelector(".extras #current-question").textContent = currQ;
-         e.target.closest(".quiz-main").style.backgroundColor = "#292a2e";
-         console.log(leftgone);
+         setMove(currQ * 100);
       }
    };
    const prevBtnClickHandler = (e) => {
-      if (leftgone - 100 / questions.length >= -2) {
-         e.target
-            .closest(".quiz-main")
-            .querySelector(
-               "#questions-wrapper"
-            ).style.transform = `translateX(-${
-            leftgone - 100 / questions.length
-         }%)`;
-         leftgone = leftgone - 100 / questions.length;
+      // if (leftgone - 100 / questions.length >= -2) {
+      //    e.target
+      //       .closest(".quiz-main")
+      //       .querySelector(
+      //          "#questions-wrapper"
+      //       ).style.transform = `translateX(-${
+      //       leftgone - 100 / questions.length
+      //    }%)`;
+      //    leftgone = leftgone - 100 / questions.length;
+      //    currQ--;
+      //    e.target
+      //       .closest(".quiz-main")
+      //       .querySelector(".extras #current-question").textContent = currQ;
+      //    console.log(leftgone);
+      //    e.target.closest(".quiz-main").style.backgroundColor = "#292a2e";
+      // }
+      if (currQ - 1 >= 0) {
          currQ--;
-         e.target
-            .closest(".quiz-main")
-            .querySelector(".extras #current-question").textContent = currQ;
-         console.log(leftgone);
-         e.target.closest(".quiz-main").style.backgroundColor = "#292a2e";
+         setMove(currQ * 100);
       }
    };
    const revealAnswer = (e) => {
-      e.target
-         .closest(".quiz-main")
-         .querySelectorAll(".question")
-         [currQ - 1].querySelectorAll(".option-section .option")
-         .forEach((element) => {
-            // console.log(.textContent)
-            const el = element.querySelector("span");
-            if (el.textContent === questions[currQ - 1].correctAnswer) {
-               console.log("hahah");
-               el.style.backgroundColor = "#285427";
-            } else {
-               el.style.backgroundColor = "#322020";
-            }
-         });
+      ras++;
+      // e.target
+      //    .closest(".quiz-main")
+      //    .querySelectorAll(".question")
+      //    [currQ - 1].querySelectorAll(".option-section .option")
+      //    .forEach((element) => {
+      //       // console.log(.textContent)
+      //       const el = element.querySelector("span");
+      //       if (el.textContent === questions[currQ - 1].correctAnswer) {
+      //          console.log("hahah");
+      //          el.style.backgroundColor = "#285427";
+      //       } else {
+      //          el.style.backgroundColor = "#322020";
+      //       }
+      //    });
    };
    const isSuccess = (e) => {
       if (e === 1) {
@@ -101,35 +111,44 @@ const QuizPlayground = () => {
          id="single-quiz"
          className="vh-100 d-flex justify-content-center align-items-center"
       >
-         <div className="quiz-main d-flex flex-column">
-            <div className="extras d-flex justify-content-between">
-               <p className="text-white fs-5 text-white-50">
+         <div
+            className="quiz-main d-flex"
+            id="quiz-main"
+            // style={{ width: `${questions.length * 100}%`, flex: "1" }}
+         >
+            <div className="extras d-flex justify-content-start align-items-center px-4 w-100">
+               <p className="text-white fs-5 text-white-50 w-25 user-select-none">
                   <span id="current-question" className="text-info">
-                     {currQ}
+                     {currQ + 1}
                   </span>{" "}
                   / <span id="total-question">{questions.length}</span>
                </p>
-               <h3 className="text-white fs-4">{data.name} Quizes</h3>
-               <FontAwesomeIcon
-                  icon={faEye}
-                  className="fs-4 text-white"
-                  onClick={revealAnswer}
-               />
+               <h3 className="text-white fs-4 text-center w-75 user-select-none">{data.name} Quizes</h3>
             </div>
-            <div
+            {questions.map((question, id) => (
+               <SingleQuestion
+                  question={question}
+                  ql={questions.length}
+                  isSuccess={isSuccess}
+                  serialNumber={id}
+                  move={move}
+               />
+            ))}
+            {/* <div
                style={{ width: `${questions.length * 100}%`, flex: "1" }}
                className="d-flex"
                id="questions-wrapper"
             >
-               {questions.map((question) => (
+               {questions.map((question, id) => (
                   <SingleQuestion
+
                      question={question}
                      ql={questions.length}
                      isSuccess={isSuccess}
                   />
                ))}
-            </div>
-            <div className="buttons d-flex align-items-end justify-content-center">
+            </div> */}
+            <div className="buttons d-flex align-items-start justify-content-center align-self-end w-100">
                <Button
                   className="mx-3"
                   variant="warning"
--- src/components/QuizPlayground/SingleQuestion/SingleQuestion.js
+++ src/components/QuizPlayground/SingleQuestion/SingleQuestion.js
@@ -1,18 +1,14 @@
 import "./SingleQuestion.css";
 import React, { useState } from "react";
-
-
-
-
-
-
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 const SingleQuestion = (props) => {
    const question = props.question;
    const answer = props.question.correctAnswer;
    /*
+      ## Data Model ##
+
       correctAnswer: "29"
       id:"62e531f2d2c8922246db90e9"
       options: [
@@ -20,29 +16,32 @@ const SingleQuestion = (props) => {
       ]
       question: "<p>If&nbsp;you have a var age"
    */
-   
-   // Checking wether user selected any option or not 
-   const [selected, setSelected] = useState(false); 
 
+   /*
+      ** Checking wether user selected any option and is that correct or not
 
-   const optionSelectionHandler = e => {
+      Started by assingning an empty string to the state.
+      Empty string means there is no value at the initial stage which can also be referred as neutral state.
+      Neutral state simply means user has't selected anything.
+      This makes the background color of every question to neutral.
 
-      setSelected(true);
-      console.log("Set Selected")
+      This state will be changed after selecting an option by user.
+      After selecting an option, it will contain "2" or "1" according to the right or wrong answer respectively after checking the answer.
+      This state will manupulate the background color of each question respective to right or wrong.
+   */
+   const [checkAnswer, setCheckAnswer] = useState("");
+
+   const optionSelectionHandler = (e) => {
+      setCheckAnswer(true);
+      console.log("Set Selected");
 
       const selectedOption = e.target.dataset.option;
-      const correctAnswer = e.target.closest('.question').dataset.answer;
+      const correctAnswer = e.target.closest(".question").dataset.answer;
 
       // Right Answer
-      if(selectedOption === correctAnswer){
-
-      }
+      if (selectedOption === correctAnswer) setCheckAnswer(2);
       // Wrong Answer
-      else{
-
-      }
-
-
+      else setCheckAnswer(1);
       // e.target.closest('.option-section').querySelectorAll('.option span').forEach(element => {
       //    if(element.textContent === question.correctAnswer){
       //       element.style.backgroundColor = '#285427'
@@ -61,15 +60,46 @@ const SingleQuestion = (props) => {
       //    props.isSuccess(0)
       //    console.log("Wrong")
       // }
-   }
-   return (
-      <div className="question d-flex justify-content-between align-items-center flex-column" data-answer={question.correctAnswer} >
-         <div className="question-status">
+   };
 
+   const revealAnswer = (e) => {
+      // setCheckAnswer((prev) => (prev === 1 ? 1 : prev === 2 ? 2 : 3));
+      setCheckAnswer(3);
+   };
+   return (
+      <div
+         className={`question d-flex justify-content-between align-items-center flex-column align-self-center ${
+            /*
+               ** Checking and adding background color according to the right or wrong answer.
+
+               If the answer is correct means "2", "right-answer-overlay" class will be added.
+               If the answer is wrong means "1", "wrong-answer-overlay" class will be added.
+             */
+            checkAnswer === 2
+               ? "right-answer-overlay"
+               : checkAnswer === 1
+               ? "wrong-answer-overlay"
+               : checkAnswer === 3
+               ? "reveal-answer-overlay"
+               : ""
+         }`}
+         data-answer={question.correctAnswer}
+         style={{
+            transform: `translateX(${props.serialNumber * 100 - props.move}%)`,
+         }}
+      >
+         <FontAwesomeIcon
+            icon={faEye}
+            className={`reveal-answer fs-4 text-white ${
+               checkAnswer !== "" ? "d-none" : ""
+            }`}
+            onClick={revealAnswer}
+         />
+         <div className="question-section user-select-none">
+            {question.question.replace(/(<([^>]+)>)/gi, "")}
          </div>
-         <div className="question-section">{question.question}</div>
          <div className="option-section w-100 d-flex flex-column">
-            <div className={`${selected ? 'options-overlay' : ''}`}></div>
+            <div className={`${checkAnswer ? "options-overlay" : ""}`}></div>
             {question.options.map((option, i) => (
                <div className="option my-1 h-100" key={i}>
                   <label className="h-100">
@@ -79,7 +109,28 @@ const SingleQuestion = (props) => {
                         name={question.id}
                         value="HTML"
                      />
-                     <span className={`text-white fs-6 d-flex w-100 h-100 align-items-center px-5 ${selected ? (option === answer ? 'right-answer' : 'wrong-answer') : ''}`} onClick={optionSelectionHandler} data-option={option} data-correct={answer === option ? "true" : "false"}>{option}</span>
+                     <span
+                        className={`text-white fs-6 d-flex w-100 h-100 align-items-center px-5 ${
+                           /*
+                              ** Checking and adding option's background color according to the right or wrong options
+
+                              If the option is correct, "right-answer" class will be added.
+                              If the option is wrong, "wrong-answer" class will be added.
+
+                              Note: State will be changed by clicking one of those options.
+                           */
+                           checkAnswer
+                              ? option === answer
+                                 ? "right-answer"
+                                 : "wrong-answer"
+                              : ""
+                        }`}
+                        onClick={optionSelectionHandler}
+                        data-option={option}
+                        data-correct={answer === option ? "true" : "false"}
+                     >
+                        {option}
+                     </span>
                   </label>
                   <br></br>
                </div>
